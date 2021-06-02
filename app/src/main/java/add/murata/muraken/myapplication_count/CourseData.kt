286e14a50cc93dc9a0bd76cea056065dfad496e8
kento <kento010610@icloud.com>
2021-06-03 02:00:16 +0900

    形作り

---
+++ app/src/main/java/add/murata/muraken/myapplication_count/CourseData.kt
@@ -0,0 +1,5 @@
+package add.murata.muraken.myapplication_count
+
+data class CourseData(val characterImageResourse: Int,
+                      val courseName: String,
+                      val decription: String)
